e9c2a3f943ee4a30c1920cc9db29443f58bcd4be
Alvaro Jurado <quickalvaro@gmail.com>
2024-10-02 23:18:00 +0200

    cambios raycasting

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -40,27 +40,6 @@
 # define BLOCKSIZE 64
 # define TORADIANS (M_PI / 180.0)
 
-typedef struct s_ray
-{
-	char	player_dir;
-	double	dir_x;
-	double	dir_y;
-	int		map_x;
-	int		map_y;
-	double	side_dist_x;
-	double	side_dist_y;
-	double	delta_dist_x;
-	double	delta_dist_y;
-	int		step_x;
-	int		step_y;
-	int		side;
-	int		draw_start;
-	int		draw_end;
-	double	perp_wall_dist;
-	int		line_height;
-	double	wall_x;
-}	t_ray;
-
 // Estructura para almacenar coordenadas simples (x, y)
 typedef struct  s_coords
 {
--- src/raycasting/raycasting.c
+++ src/raycasting/raycasting.c
@@ -1,106 +1,97 @@
 #include "cub3d.h"
 
-static void	calculate_heights(t_ray *ray)
+static void	picasso(int col, t_cub3data *data, mlx_image_t *img, int rest)
 {
-	if (!(ray->side))
-		ray->perp_wall_dist = ray->side_dist_x - ray->delta_dist_x;
-	else
-		ray->perp_wall_dist = ray->side_dist_y - ray->delta_dist_y;
-	ray->line_height = (int) HEIGHT / ray->perp_wall_dist;
-	ray->draw_start = -(ray->line_height) / 2 + HEIGHT / 2;
-	if (ray->draw_start < 0)
-		ray->draw_start = 0;
-	ray->draw_end = ray->line_height / 2 + HEIGHT / 2;
-	if (ray->draw_end >= HEIGHT)
-		ray->draw_end = HEIGHT - 1;
-}
+	int			i;
+	int			col2;
 
-static void	execute_dda(t_data *data, t_ray *ray)
-{
-	int		hit;
-
-	hit = 0;
-	(void) data;
-	while (!hit)
-	{
-		if (ray->side_dist_x < ray->side_dist_y)
-		{
-			ray->side_dist_x += ray->delta_dist_x;
-			ray->map_x += ray->step_x;
-			ray->side = 0;
-		}
-		else
-		{
-			ray->side_dist_y += ray->delta_dist_y;
-			ray->map_y += ray->step_y;
-			ray->side = 1;
-		}
-		if (data->map[ray->map_x][ray->map_y] != '0'
-			&& !ft_strchr("NSWE", data->map[ray->map_x][ray->map_y]))
-				hit = 1;
-	}
-}
-
-static void	calculate_euclidean_distance(t_ray *ray, t_player *player)
-{
-	if (ray->dir_x < 0)
-	{
-		ray->step_x = -1;
-		ray->side_dist_x = (player->pos_x - ray->map_x) * ray->delta_dist_x;
-	}
-	else
-	{
-		ray->step_x = 1;
-		ray->side_dist_x = (ray->map_x + 1.0 - player->pos_x)
-			* ray->delta_dist_x;
-	}
-	if (ray->dir_y < 0)
-	{
-		ray->step_y = -1;
-		ray->side_dist_y = (player->pos_y - ray->map_y) * ray->delta_dist_y;
-	}
-	else
-	{
-		ray->step_y = 1;
-		ray->side_dist_y = (ray->map_y + 1.0 - player->pos_y)
-			* ray->delta_dist_y;
-	}
-}
-
-static void	set_raycast_info(t_data *data, int x)
-{
-	double		camera_x;
-	t_ray		*ray;
-	t_player	*player;
-
-	ray = data->ray;
-	player = data->player;
-	camera_x = 2 * x / (double) WIDTH - 1;
-	ray->dir_x = player->dir_x + player->plane_x * camera_x;
-	ray->dir_y = player->dir_y + player->plane_y * camera_x;
-	ray->map_x = (int) player->pos_x;
-	ray->map_y = (int) player->pos_y;
-	if (ray->dir_x == 0)
-		ray->delta_dist_x = INFINITE;
-	else
-		ray->delta_dist_x = fabs(1 / ray->dir_x);
-	if (ray->dir_y == 0)
-		ray->delta_dist_y = INFINITE;
-	else
-		ray->delta_dist_y = fabs(1 / ray->dir_y);
-	calculate_euclidean_distance(ray, player);
-}
-
-void	render_game(t_data *data)
-{
-	int		i;
-
+	data->aux = (HEIGHT / (data->aux / BLOCKSIZE));
+	data->aux = (HEIGHT - data->aux) / 2;
 	i = -1;
-	while (++i < WIDTH)
+	col2 = rest * img->width / BLOCKSIZE;
+	while (++i < HEIGHT - 1)
 	{
-		set_raycast_info(data, i);
-		execute_dda(data, data->ray);
-		calculate_heights(data->ray);
-		textures_calculation(data, data->ray, data->player, i);
+		if (i < data->aux)
+			put_rgbcolor(&(data->full_img->pixels[(i * WIDTH + col) * 4]), \
+			data->color.ceiling, 0);
+		else if (i < HEIGHT - data->aux - 1)
+			put_rgbimg(&(data->full_img->pixels[(i * WIDTH + col) * 4]), \
+			&img->pixels[(col2 + (i - (int)(data->aux)) * \
+			img->height / (HEIGHT - 2 * (int)data->aux) * img->width) * 4]);
+		else
+			put_rgbcolor(&(data->full_img->pixels[(i * WIDTH + col) * 4]), \
+			data->color.floor, 0);
+		put_rgbimg(&data->full_img->pixels[(i * WIDTH + col + 1) * 4], \
+		&data->full_img->pixels[(i * WIDTH + col) * 4]);
+	}
+}
+
+static void	ft_wall_direction(t_cub3data *data, t_coords p, int d, double iter)
+{
+	int	rest_y;
+	int	rest_x;
+	int	aux;
+
+	aux = (p.y * data->width * BLOCKSIZE + p.x) * 4;
+	rest_x = p.x % BLOCKSIZE;
+	rest_y = p.y % BLOCKSIZE;
+	data->aux = d;
+	if (ft_iswall(p, data) == 2)
+	{
+		if ((rest_x <= 2 && data->map_close.img[aux - 2 * 4] != 255) || \
+		(rest_x >= BLOCKSIZE - 2 && data->map_close.img[aux + 2 * 4] != 255))
+			picasso(iter * (WIDTH / ANGLE), data, data->wall.door, rest_y);
+		else
+			picasso(iter * (WIDTH / ANGLE), data, data->wall.door, rest_x);
+		return ;
+	}
+	if (rest_x <= 2 && data->map_close.img[aux - 2 * 4] != 255)
+		picasso(iter * (WIDTH / ANGLE), data, data->wall.e, rest_y);
+	else if (rest_x >= BLOCKSIZE - 2 && data->map_close.img[aux + 2 * 4] != 255)
+		picasso(iter * (WIDTH / ANGLE), data, data->wall.w, rest_y);
+	else if (rest_y <= 2)
+		picasso(iter * (WIDTH / ANGLE), data, data->wall.s, rest_x);
+	else if (rest_y >= BLOCKSIZE - 2)
+		picasso(iter * (WIDTH / ANGLE), data, data->wall.n, rest_x);
+}
+
+static int	ft_take_dist(t_cub3data *data, double iter, t_coords *p)
+{
+	int			dist;
+	double		fix;
+	double		cos_c;
+	double		sin_c;
+	int			iswall;
+
+	dist = 1;
+	fix = cos((iter + 180 - (ANGLE / 2)) * TORADIANS);
+	cos_c = (cos(data->player.angle + (iter - (ANGLE / 2)) * TORADIANS)) / fix;
+	sin_c = (sin(data->player.angle + (iter - (ANGLE / 2)) * TORADIANS)) / fix;
+	p->x = data->player.pos.x + dist * cos_c;
+	p->y = data->player.pos.y + dist * sin_c;
+	while (p->x >= 0 && p->y >= 0 && p->x < WIDTH * BLOCKSIZE && \
+	p->y < HEIGHT * BLOCKSIZE && ++dist)
+	{
+		iswall = ft_iswall(*p, data);
+		if ((iswall == 2 && data->door_open == 0) || iswall == 1)
+			break ;
+		p->x = data->player.pos.x + dist * cos_c;
+		p->y = data->player.pos.y + dist * sin_c;
+	}
+	return (dist);
+}
+
+void	raycasting(t_cub3data *data, t_coords pos)
+{
+	static const double	iter_variation = 1.0 / (WIDTH / ANGLE);
+	double				iter;
+	int					dist;
+
+	iter = 0;
+	while (iter <= ANGLE)
+	{
+		dist = ft_take_dist(data, iter, &pos);
+		ft_wall_direction(data, pos, dist, iter);
+		iter += iter_variation;
 	}
 }
